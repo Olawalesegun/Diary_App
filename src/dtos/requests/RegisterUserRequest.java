10fb708e7157f296e811343c73a3a8c7a960935d
Olawalesegun <olawalesegun0@gmail.com>
2023-07-08 22:36:45 +0100

    The Register User Request DTO, serves the purpose of data collection for registration

--- src/dtos/requests/RegisterUserRequest.java
+++ src/dtos/requests/RegisterUserRequest.java
@@ -1,2 +1,29 @@
-package dtos.requests;public class RegisterUserRequest {
+package dtos.requests;
+
+public class RegisterUserRequest {
+    private String userName;
+    private String email;
+    private String password;
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
