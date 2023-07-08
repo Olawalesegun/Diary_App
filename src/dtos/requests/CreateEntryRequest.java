f6b0b28cc43def416a69a90ead612643ac8a6a32
Olawalesegun <olawalesegun0@gmail.com>
2023-07-08 22:18:21 +0100

    The Create Entry DTO, serves the purpose of data collection for the creation of Entry

--- src/dtos/requests/CreateEntryRequest.java
+++ src/dtos/requests/CreateEntryRequest.java
@@ -1,2 +1,40 @@
-package dtos.requests;public class CreateEntryRequest {
+package dtos.requests;
+
+public class CreateEntryRequest {
+    private String entryTitle;
+    private String entryBody;
+    private String diaryName;
+    private String diaryBody;
+
+    public String getEntryTitle() {
+        return entryTitle;
+    }
+
+    public void setEntryTitle(String entryTitle) {
+        this.entryTitle = entryTitle;
+    }
+
+    public String getEntryBody() {
+        return entryBody;
+    }
+
+    public void setEntryBody(String entryBody) {
+        this.entryBody = entryBody;
+    }
+
+    public String getDiaryName() {
+        return diaryName;
+    }
+
+    public void setDiaryName(String diaryName) {
+        this.diaryName = diaryName;
+    }
+
+    public String getDiaryBody() {
+        return diaryBody;
+    }
+
+    public void setDiaryBody(String diaryBody) {
+        this.diaryBody = diaryBody;
+    }
 }
