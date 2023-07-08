c5b8ba0ca35e29b356eafd8185fdc1f966577dd6
Olawalesegun <olawalesegun0@gmail.com>
2023-07-08 22:00:25 +0100

    User Repo: an interface to be implemented, no Pojos, strictly interfaces

--- src/data/repositories/EntryRepository.java
+++ src/data/repositories/EntryRepository.java
@@ -3,7 +3,6 @@ package data.repositories;
 import data.models.Entry;
 public interface EntryRepository {
     Entry save(Entry entry);
-
     Entry findBy(String entryTitle);
     Entry findBy(int entryID);
     void deleteBy(int entryID);
