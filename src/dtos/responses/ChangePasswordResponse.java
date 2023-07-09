23f6c40363bae7235712446fc1e53e808b616278
Olawalesegun <olawalesegun0@gmail.com>
2023-07-09 22:13:23 +0100

    changed password response: this response notifies users the status of changed password

--- src/dtos/responses/ChangePasswordResponse.java
+++ src/dtos/responses/ChangePasswordResponse.java
@@ -1,2 +1,40 @@
-package dtos.responses;public class changePasswordResponse {
+package dtos.responses;
+
+public class ChangePasswordResponse {
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    private String userName;
+    private String oldPassword;
+    private String newPassword;
+
+    public String toString(){
+        return String.format("""
+                ================================================
+                   You have successfully changed your password
+                ================================================
+                """
+        );
+    }
 }
