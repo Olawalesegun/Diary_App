1c5f88ac1b3a88ce27ece58a3e171c3e4f2348fe
Olawalesegun <olawalesegun0@gmail.com>
2023-07-09 22:26:21 +0100

    user name already exists exception: this throws an exception when username already exist

--- src/exceptions/UserNameDoesNotExistException.java
+++ src/exceptions/UserNameDoesNotExistException.java
@@ -1,2 +1,7 @@
-package exceptions;public class UserNameDoesNotExistException {
+package exceptions;
+
+public class UserNameDoesNotExistException extends RuntimeException{
+    public UserNameDoesNotExistException(String message){
+        super(message);
+    }
 }
