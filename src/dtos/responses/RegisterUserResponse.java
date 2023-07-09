97635f3e47005602da79be27f277fa843f0e4005
Olawalesegun <olawalesegun0@gmail.com>
2023-07-09 22:15:02 +0100

    user registration response: this response notifies users the status when they register

--- src/dtos/responses/RegisterUserResponse.java
+++ src/dtos/responses/RegisterUserResponse.java
@@ -1,2 +1,21 @@
-package dtos.responses;public class RegisterUserResponse {
+package dtos.responses;
+
+public class RegisterUserResponse {
+    private String userName;
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String toString(){
+        return String.format("""
+                ======================================================
+                   You have successfully registered %s
+                ======================================================
+                """,getUserName()
+        );
+    }
+
+
 }
