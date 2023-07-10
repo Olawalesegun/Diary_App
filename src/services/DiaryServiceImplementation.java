300cd1db18b66a1b40fe489f905f848b5701e1c1
Olawalesegun <olawalesegun0@gmail.com>
2023-07-10 23:08:27 +0100

    Diary Service Impl

--- src/services/DiaryServiceImplementation.java
+++ src/services/DiaryServiceImplementation.java
@@ -1,2 +1,60 @@
-package services;public class DiaryServiceImplementation {
+package services;
+
+import data.models.Diary;
+import data.repositories.DiaryRepository;
+import data.repositories.DiaryRepositoryImpl;
+import dtos.requests.*;
+import dtos.responses.*;
+
+public class DiaryServiceImplementation implements DiaryService{
+    DiaryRepository diaryRepository = new DiaryRepositoryImpl();
+    @Override
+    public CreateDiaryResponse createDiary(CreateDiaryRequest createDiaryRequest) {
+        Diary diary = new Diary();
+        diary.setUserName(createDiaryRequest.getUserName());
+        diary.setDiaryName(createDiaryRequest.getDiaryName());
+        diaryRepository.save(diary);
+        CreateDiaryResponse createDiaryResponse = new CreateDiaryResponse();
+        createDiaryResponse.setDiaryName(createDiaryRequest.getDiaryName());
+        createDiaryResponse.setDateEntered(createDiaryRequest.getTimeEntered());
+        createDiaryResponse.setTimeEntered();
+        return createDiaryResponse;
+    }
+
+    @Override
+    public DeleteDiaryResponse deleteDiary(DeleteDiaryResponse deleteDiaryResponse) {
+        return null;
+    }
+
+    @Override
+    public CreateEntryResponse createEntry(CreateDiaryRequest createDiaryRequest) {
+        return null;
+    }
+
+    @Override
+    public UpdateEntryResponse updateEntry(UpdateEntryRequest updateEntryRequest) {
+        return null;
+    }
+
+    @Override
+    public DeleteDiaryResponse deleteEntry(DeleteDiaryRequest deleteDiaryRequest) {
+        return null;
+    }
+
+    @Override
+    public FindEntryResponse findEntry(FindEntryRequest findDiaryRequest) {
+
+        return null;
+    }
+
+    @Override
+    public FindDiaryResponse findDiary(FindDiaryRequest findDiaryRequest) {
+        /*diaryRepository.findBy(findDiaryRequest)*/
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return diaryRepository.count();
+    }
 }
---
+++ test/services/DiaryServiceImplementationTest.java
@@ -0,0 +1,6 @@
+package services;
+
+import static org.junit.jupiter.api.Assertions.*;
+class DiaryServiceImplementationTest {
+  
+}
