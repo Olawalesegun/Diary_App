2df63579cd04e5cdddd907cb0a887740398aa23c
Olawalesegun <olawalesegun0@gmail.com>
2023-07-10 23:10:07 +0100

    User Service Pojos

--- src/services/UserService.java
+++ src/services/UserService.java
@@ -1,2 +1,19 @@
-package services;public interface UserService {
+package services;
+
+import dtos.requests.*;
+import dtos.responses.*;
+import exceptions.PasswordDoesNotMatchException;
+import exceptions.UserLoginDetailsIncorrectException;
+import exceptions.UserNameDoesNotExistException;
+
+public interface UserService {
+    RegisterUserResponse registerUser(RegisterUserRequest registerUserRequest);
+    LoginUserResponse loginUser(LoginUserRequest loginUserRequest) throws UserLoginDetailsIncorrectException, UserNameDoesNotExistException, PasswordDoesNotMatchException;
+    ChangePasswordResponse changePassword(ChangePasswordRequest changePasswordRequest);
+
+    boolean isLoggedIn(String userName);
+    CreateDiaryResponse createDiary(CreateDiaryRequest createDiaryRequest);
+    CreateEntryResponse createEntry(CreateEntryRequest createEntryRequest);
+    DeleteDiaryResponse deleteEntry(DeleteDiaryRequest deleteEntryRequest);
+    long count();
 }
