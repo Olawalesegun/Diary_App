e527640353c6cbf4906bc9d580a4f5dd79709821
Olawalesegun <olawalesegun0@gmail.com>
2023-07-10 23:08:52 +0100

    Diary Service Pojos

--- src/services/DiaryService.java
+++ src/services/DiaryService.java
@@ -1,2 +1,16 @@
-package services;public interface DiaryService {
+package services;
+
+import dtos.requests.*;
+import dtos.responses.*;
+
+public interface DiaryService {
+
+    CreateDiaryResponse createDiary(CreateDiaryRequest createDiaryRequest);
+    DeleteDiaryResponse deleteDiary(DeleteDiaryResponse deleteDiaryResponse);
+    CreateEntryResponse createEntry(CreateDiaryRequest createDiaryRequest);
+    UpdateEntryResponse updateEntry(UpdateEntryRequest updateEntryRequest);
+    DeleteDiaryResponse deleteEntry(DeleteDiaryRequest deleteDiaryRequest);
+    FindEntryResponse findEntry(FindEntryRequest findDiaryRequest);
+    FindDiaryResponse findDiary(FindDiaryRequest findDiaryRequest);
+    long count();
 }
