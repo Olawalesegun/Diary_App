11ab39604e29b3e8dafd900983bf61e405e742cc
Olawalesegun <olawalesegun0@gmail.com>
2023-07-11 23:25:06 +0100

    Diary Does Not Exist

--- src/exceptions/DiaryDoesNotExist.java
+++ src/exceptions/DiaryDoesNotExist.java
@@ -1,2 +1,7 @@
-package exceptions;public class DiaryDoesNotExist {
+package exceptions;
+
+public class DiaryDoesNotExist extends RuntimeException{
+    public DiaryDoesNotExist(String message){
+        super(message);
+    }
 }
