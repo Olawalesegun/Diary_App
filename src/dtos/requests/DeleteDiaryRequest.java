0bdff94a498cf7303028aee3b225978d04c44b7d
Olawalesegun <olawalesegun0@gmail.com>
2023-07-12 22:42:00 +0100

    Delete Diary Request

--- src/dtos/requests/DeleteDiaryRequest.java
+++ src/dtos/requests/DeleteDiaryRequest.java
@@ -3,4 +3,19 @@ package dtos.requests;
 public class DeleteDiaryRequest {
     private String userName;
     private String diaryName;
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getDiaryName() {
+        return diaryName;
+    }
+
+    public void setDiaryName(String diaryName) {
+        this.diaryName = diaryName;
+    }
 }
