066d78cc89f6eac0a7a3ef8ff174d5418ebc38b3
Olawalesegun <olawalesegun0@gmail.com>
2023-07-12 22:42:37 +0100

    Find Diary Request

--- src/dtos/requests/FindDiaryRequest.java
+++ src/dtos/requests/FindDiaryRequest.java
@@ -1,4 +1,23 @@
 package dtos.requests;
 
 public class FindDiaryRequest {
+
+    private String diaryName;
+    private String userName;
+
+    public String getDiaryName() {
+        return diaryName;
+    }
+
+    public void setDiaryName(String diaryName) {
+        this.diaryName = diaryName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
