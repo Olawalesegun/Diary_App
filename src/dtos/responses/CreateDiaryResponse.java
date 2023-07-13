08feba35c57419ee0913deeba1672d96129931ae
Olawalesegun <olawalesegun0@gmail.com>
2023-07-13 23:29:30 +0100

    Create Diary Response

--- src/dtos/responses/CreateDiaryResponse.java
+++ src/dtos/responses/CreateDiaryResponse.java
@@ -1,2 +1,30 @@
-package dtos.responses;public class CreateDiaryResponse {
+package dtos.responses;
+
+import java.time.LocalDateTime;
+
+public class CreateDiaryResponse {
+    private String dateAndTimeEntered;
+    private String diaryName;
+
+    public void setDateAndTimeEntered(String dateEntered) {
+        this.dateAndTimeEntered = dateEntered;
+    }
+    public String getDateAndTimeEntered() {
+        return dateAndTimeEntered;
+    }
+    public String getDiaryName() {
+        return diaryName;
+    }
+
+    public void setDiaryName(String diaryName) {
+        this.diaryName = diaryName;
+    }
+
+    @Override
+    public String toString() {
+        return "CreateDiaryResponse{" +
+                "dateEntered='" + dateAndTimeEntered + '\'' +
+                ", diaryName='" + diaryName + '\'' +
+                '}';
+    }
 }
