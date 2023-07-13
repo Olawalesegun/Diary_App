ad128b756a9084b55f578ae663a82b9e9979928a
Olawalesegun <olawalesegun0@gmail.com>
2023-07-13 23:26:11 +0100

    Change Password DTO

--- src/dtos/responses/ChangePasswordResponse.java
+++ src/dtos/responses/ChangePasswordResponse.java
@@ -1,34 +1,28 @@
 package dtos.responses;
 
 public class ChangePasswordResponse {
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getOldPassword() {
-        return oldPassword;
-    }
-
-    public void setOldPassword(String oldPassword) {
-        this.oldPassword = oldPassword;
-    }
-
-    public String getNewPassword() {
-        return newPassword;
-    }
-
-    public void setNewPassword(String newPassword) {
-        this.newPassword = newPassword;
-    }
 
     private String userName;
     private String oldPassword;
     private String newPassword;
-
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getOldPassword() {
+        return oldPassword;
+    }
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+    public String getNewPassword() {
+        return newPassword;
+    }
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
     public String toString(){
         return String.format("""
                 ================================================
