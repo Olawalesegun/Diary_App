8ffcddd40bcedc33d57764acff5870c3d4cb8f6b
Olawalesegun <olawalesegun0@gmail.com>
2023-07-16 22:53:58 +0100

    User POJO

--- src/data/models/User.java
+++ src/data/models/User.java
@@ -5,6 +5,38 @@ import java.time.LocalDateTime;
 public class User {
     private String userName;
     private String diaryName;
+    private String email;
+
+    private boolean isLoggedIn;
+    public boolean isLoggedIn() {
+        return isLoggedIn;
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        isLoggedIn = loggedIn;
+    }
+
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
     private final LocalDateTime dateAndTimeForDiaryCreation = LocalDateTime.now();
 
     public String getUserName() {
