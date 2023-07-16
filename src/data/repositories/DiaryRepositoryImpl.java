919bc3578cf4eb79bf0da1eb88f082323348b848
Olawalesegun <olawalesegun0@gmail.com>
2023-07-16 22:56:33 +0100

    Diary Repositories Implementation

--- src/data/repositories/DiaryRepositoryImpl.java
+++ src/data/repositories/DiaryRepositoryImpl.java
@@ -30,20 +30,49 @@ public class DiaryRepositoryImpl implements DiaryRepository{
         return null;
     }
 
+    public Diary findBy(Diary diary){
+        for(Diary diary1: diaries){
+            if(diary1.getDiaryName().equals(diary.getDiaryName()) &&
+            diary1.getUserName().equals(diary.getUserName())){
+                return diary;
+            }
+        }
+        return null;
+    }
+
+  /*  @Override
+    public boolean equals(Object anObject) {
+        if (this == anObject) {
+            return true;
+        } else {
+            boolean var10000;
+            if (anObject instanceof String) {
+                String aString = (String)anObject;
+                if ((!COMPACT_STRINGS || this.coder == aString.coder) && StringLatin1.equals(this.value, aString.value)) {
+                    var10000 = true;
+                    return var10000;
+                }
+            }
+
+            var10000 = false;
+            return var10000;
+        }
+    }*/
     @Override
-    public void delete(String diaryName) {
-        Diary findDiary = findBy(diaryName);
-        diaries.remove(findDiary);
+    public void delete(Diary diary) {
+        Diary findDiary = findBy(diary.getDiaryName());
+        if(findDiary.getDiaryName().equals(diary.getDiaryName()) &&
+        findDiary.getUserName() == diary.getUserName()){
+            diaries.remove(diary);
+        }
+
     }
 
     public void update(Diary diaryName, Diary newDiaryName){
         int indexOfDiary = diaries.indexOf(diaryName);
         diaries.set(indexOfDiary, newDiaryName);
     }
-    @Override
-    public void delete(Diary diary) {
 
-    }
 
     @Override
     public long count() {
