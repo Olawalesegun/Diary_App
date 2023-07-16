a621aa60031ec9631f03f8d3103850bbcb0f3504
Olawalesegun <olawalesegun0@gmail.com>
2023-07-16 22:56:56 +0100

    Diary Repository interface

--- src/data/repositories/DiaryRepository.java
+++ src/data/repositories/DiaryRepository.java
@@ -4,7 +4,7 @@ import data.models.Diary;
 public interface DiaryRepository {
     Diary save(Diary diary);
     Diary findBy(String diaryName);
-    void delete(String diaryName);
+   // void delete(String diaryName, String userName);
     void delete(Diary diary);
     long count();
 }
